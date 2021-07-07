71ea4cbe8db7986fe3b7f4943bf30adcf007fede
moxie_www3 <moxiewww3@MacBook-Pro.local>
2021-07-07 12:56:48 -0500

    create message.js

---
+++ messages.js
@@ -0,0 +1,6 @@
+/**
+ *  Mixed Messages: Build a message generator program. 
+ *  The message will be made up of three different pieces of data,
+ *  Date, Time and the message. 
+ */
+console.log('Hey, Let\'s start the project.' );
